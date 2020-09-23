5f0a58dfe7aa2a358e3a6a91ab322eb8f175ba25
Kashish708 <s.kashish0123@gmail.com>
2020-09-23 11:46:06 -0400

    Commit message here

---
+++ src/AddressBook.java
@@ -0,0 +1,16 @@
+public class AddressBook {
+
+    private static BuddyInfo buddy;
+
+    public static void main(String[] args) {
+        buddy = new BuddyInfo();
+        System.out.println("AddressBook");
+    }
+
+    private void addBuddy(){
+    }
+
+    private void removeBuddy(){
+    }
+
+}
