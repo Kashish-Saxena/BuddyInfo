143cfa45b70e346c835cde542ff2ac5bc0549824
Kashish708 <s.kashish0123@gmail.com>
2020-10-02 08:01:56 -0400

    Adds and Removes a buddy from the buddies list

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,17 +1,30 @@
+import java.util.*;
 public class AddressBook {
 
     private static BuddyInfo buddy;
+    private static List<BuddyInfo> buddies;
+
+    public AddressBook() {
+        buddies = new ArrayList<>();
+    }
+
+    private void addBuddy(BuddyInfo b){
+        if (b != null)
+            buddies.add(b);
+    }
+
+    private BuddyInfo removeBuddy(int index) {
+        if (index >= 0 && index < buddies.size())
+            buddies.remove(index);
+        return null;
+    }
 
     public static void main(String[] args) {
-        buddy = new BuddyInfo();
-        System.out.println("Address Book");
+        buddy = new BuddyInfo("Jack", "Carleton", 613);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(0);
 
-    }
-
-    private void addBuddy(){
-    }
-
-    private void removeBuddy(){
     }
 
 }
